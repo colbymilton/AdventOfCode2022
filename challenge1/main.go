ba977d2cbef37dead4403c44bf4c8bffa53368bd
colbymilton <miltoncolby@gmail.com>
2022-12-01 11:52:59 -0400

    Day 1 Challenge 2

--- challenge1/main.go
+++ challenge1/main.go
@@ -58,4 +58,15 @@ func main() {
 
 	// first in list should have the most calories
 	fmt.Printf("The elf with the most calories is Elf #%v with %v calories!\n", elves[0].Id, elves[0].Calories)
+
+	// first 3 in list
+	if len(elves) >= 3 {
+		e1 := elves[0]
+		e2 := elves[1]
+		e3 := elves[2]
+		sum := e1.Calories + e2.Calories + e3.Calories
+		fmt.Printf("The three elves with the most calories are elves #%v, #%v, and #%v. "+
+			"They are carrying %v, %v, and %v calories, respectfully. In total they carry %v calories. \n",
+			e1.Id, e2.Id, e3.Id, e1.Calories, e2.Calories, e3.Calories, sum)
+	}
 }
